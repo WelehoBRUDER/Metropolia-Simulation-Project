edbb2d9bb998b294718eeb8d64088847c751c625
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-03-04 11:30:05 +0200

    drawing functions

--- src/main/java/controller/SimController.java
+++ src/main/java/controller/SimController.java
@@ -19,13 +19,26 @@ public class SimController implements ISettingsControllerForM {
     private long simDelayValue = 0;
     private int wristbandChanceValue = 0;
     private ArrayList<int[]> rideProperties = new ArrayList<>();
+    private ArrayList<int[][]> rideLocations = new ArrayList<>();
+
+    private final int SERVICE_POINT_SIZE = 20;
 
     @FXML
-    private final Canvas servicePointCanvas;
+    private Canvas servicePointCanvas;
     @FXML
-    private final Canvas customerCanvas;
-    private final GraphicsContext serviceCtx = servicePointCanvas.getGraphicsContext2D();
-    private final GraphicsContext customerCtx = customerCanvas.getGraphicsContext2D();
+    private Canvas customerCanvas;
+    private GraphicsContext serviceCtx;
+    private GraphicsContext customerCtx;
+
+    // Leevin pätkä koodia
+    public int[][] circleOfElements(int area, int size, int amount){
+        int[][] coordinates = new int[amount][2];
+        for(int i = 1; i <= amount; i++){
+            coordinates[i-1][0] = (int) ((area-(0.5*size))/area * (area*Math.cos(2*Math.PI*i/amount)));
+            coordinates[i-1][1] = (int) ((area-(0.5*size))/area * (area*Math.sin(2*Math.PI*i/amount)));
+        }
+        return coordinates;
+    }
 
     public void setSimulationParameters(int simTime, int ticketBoothCount, int rideCount, int restaurantCap, long simDelay, int wristbandChance, ArrayList<int[]> rideProperties) {
         simTimeValue = simTime;
@@ -35,15 +48,17 @@ public class SimController implements ISettingsControllerForM {
         simDelayValue = simDelay;
         wristbandChanceValue = wristbandChance;
         this.rideProperties = rideProperties;
+        serviceCtx = servicePointCanvas.getGraphicsContext2D();
+        customerCtx = customerCanvas.getGraphicsContext2D();
     }
 
     public void startSim() {
-        engine = new OwnEngine(this, rideCountValue); // luodaan uusi moottorisäie jokaista simulointia varten
-        engine.setSimulationTime(simTimeValue);
-        engine.setDelay(simDelayValue);
+        //engine = new OwnEngine(this, rideCountValue); // luodaan uusi moottorisäie jokaista simulointia varten
+        //engine.setSimulationTime(simTimeValue);
+        //engine.setDelay(simDelayValue);
         clearScreen();
         drawAllServicePoints();
-        ((Thread)engine).start();
+        //((Thread)engine).start();
     }
 
     public void clearScreen() {
@@ -53,14 +68,21 @@ public class SimController implements ISettingsControllerForM {
 
     public void drawServicePoint(int x, int y) {
         serviceCtx.setFill(Color.BLUE);
-        serviceCtx.strokeRect(x, y, 20, 20);
-        serviceCtx.fillText("0", x, y + 20);
+        serviceCtx.strokeRect(x, y, SERVICE_POINT_SIZE, SERVICE_POINT_SIZE);
+        serviceCtx.fillText("0", x, y);
     }
 
     public void drawAllServicePoints() {
         // Ticket booths
         for (int i = 0; i < ticketBoothCountValue; i++) {
             drawServicePoint(15, i * 30 + 15);
+        }
+        // Create ride service points
+        rideLocations.clear();
+        int[][] cords = circleOfElements(250, SERVICE_POINT_SIZE, rideCountValue);
+        for (int i = 0; i < rideCountValue; i++) {
+            rideLocations.add(new int[][]{cords[i]});
+            drawServicePoint(cords[i][0] + 250, cords[i][1] + 250);
         }
         // Restaurant
         drawServicePoint(700, 100);
