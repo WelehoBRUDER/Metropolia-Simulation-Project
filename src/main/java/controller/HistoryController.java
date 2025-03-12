96d05d8599b109b05dd360efe29e7fffa71d0e99
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2025-03-12 15:02:14 +0200

    simple history list

--- src/main/java/controller/HistoryController.java
+++ src/main/java/controller/HistoryController.java
@@ -2,16 +2,28 @@ package controller;
 
 import database.Dao;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class HistoryController {
     private Dao dao = new Dao();
+    private int sims = 0;
+    @FXML
+    private VBox allResults;
+
+    public void setSims(int sims) {
+        this.sims = sims;
+    }
 
     @FXML
     private TextField searchBar;
 
     public void initialize(){
-        //dao.getSimId();
+        this.setSims(dao.getSimId());
+        this.createHistoryLinks();
     }
 
     public void unearth(){
@@ -24,8 +36,22 @@ public class HistoryController {
 
         ResultsController resultsController = new ResultsController();
         resultsController.visualizeResults(dao.getStaticResultsById(s), dao.getDynamicResultsById(s));
-
-
     }
 
+    public void createHistoryLinks() {
+        this.allResults.getChildren().clear();
+        for (int i = 1; i <= this.sims; i++) {
+            HBox link = new HBox();
+            link.getStyleClass().add("history-link");
+            link.setAlignment(javafx.geometry.Pos.CENTER_LEFT);
+            Label title = new Label("Simulation " + i);
+            Button clickable = new Button("View");
+            clickable.setOnAction(e -> {
+                this.searchBar.setText(title.getText().substring(11));
+                this.unearth();
+            });
+            link.getChildren().addAll(title, clickable);
+            this.allResults.getChildren().add(link);
+        }
+    }
 }
