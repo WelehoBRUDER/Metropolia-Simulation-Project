de41b887c9c74f7fa86df43d418cdb82ef73a56e
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-03-12 21:29:53 +0200

    did something :)

--- src/main/java/database/Dao.java
+++ src/main/java/database/Dao.java
@@ -1,23 +1,27 @@
 package database;
 
-import java.sql.*;
-import java.util.*;
+import distributions.Bernoulli;
+import javafx.util.Pair;
 
+import java.sql.*;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.*;
+import java.util.Date;
+
+/**
+ * Dao class connects the simulator to the database.
+ */
 public class Dao {
 
+    /**
+     * Increments the simulation id. The increments start from 1.
+     */
     public void incrementSimId(){
         Connection conn = DatabaseConnection.getConnection();
         long reallyBigNumber = System.currentTimeMillis();
-        //reallyBigNumber = 4;
-        //System.out.println(System.currentTime());
-        //String sql = "INSERT INTO simulation (sim_id, time_stamp) VALUES (NULL, " + reallyBigNumber +");";
-        String sql = "INSERT INTO simulation (sim_id) VALUES (NULL);";
-
-        //long x =777;
-        //String sql = "INSERT INTO simulation (sim_id, time_stamp) VALUES (NULL, x)";
-
-
-
+        String sql = "INSERT INTO simulation (sim_id, time_stamp) VALUES (NULL, " + reallyBigNumber +");";
 
         try {
             Statement s = conn.createStatement();
@@ -29,6 +33,11 @@ public class Dao {
         }
     }
 
+    /**
+     * Gets the latest simulation id from the database.
+     *
+     * @return The simulation id.
+     */
     public int getSimId() {
 
         Connection conn = DatabaseConnection.getConnection();
@@ -47,6 +56,86 @@ public class Dao {
         return 0;
     }
 
+    public Object getSomeBitches(){
+        return null;
+    }
+
+    /**
+     * Returns the timestamp of the simulation.
+     *
+     * @param id The id of the specific simulation.
+     * @return The timestamp of the simulation
+     */
+    public Date getSimTimestampById(int id) {
+
+        Connection conn = DatabaseConnection.getConnection();
+        String sql = "SELECT time_stamp FROM simulation WHERE sim_id = " + id + ";";
+
+        try {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+
+            rs.next();
+            //Date date = rs.getTimestamp(1);
+            //return rs.getTimestamp(1);
+            return new Date(rs.getLong(1));
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public Map<Integer, Long> getSimTimestamps() {
+
+        Connection conn = DatabaseConnection.getConnection();
+        String sql = "SELECT sim_id, time_stamp FROM simulation";
+        Map<Integer, Long> list = new HashMap<>();
+
+        try {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+
+            while(rs.next()){
+                list.put(rs.getInt(1), rs.getLong(2));
+            };
+            //return new Date(rs.getTimestamp(1).getTime());
+            return list;
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    /*
+    public LocalDateTime getSimTimeStampById(int id) {
+
+        Connection conn = DatabaseConnection.getConnection();
+        String sql = "SELECT time_stamp FROM simulation WHERE sim_id = " + id + ";";
+
+        try {
+            Statement s = conn.createStatement();
+            ResultSet rs = s.executeQuery(sql);
+
+            rs.next();
+            return Instant.ofEpochMilli(rs.getLong(id)).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        } catch (SQLException e){
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+     */
+
+    /**
+     * Is used to display static results.
+     *
+     * @param id The id of the specific results.
+     * @return The static results.
+     */
     public HashMap<String, Double> getStaticResultsById(int id) {
 
         Connection conn = DatabaseConnection.getConnection();
@@ -92,6 +181,12 @@ public class Dao {
         return results;
     }
 
+    /**
+     * Is used to display dynamic results.
+     *
+     * @param id The id of the specific results.
+     * @return The dynamic results.
+     */
     public TreeMap<String, Double> getDynamicResultsById(int id){
 
         Connection conn = DatabaseConnection.getConnection();
@@ -160,10 +255,24 @@ public class Dao {
             e.printStackTrace();
         }
         return results;
-
-
     }
 
+
+    /**
+     * Adds a service point to the database.
+     *
+     * @param simulationTime I'm not typing all this.
+     * @param readyCustomers
+     * @param ticketCustomers
+     * @param wristbandCustomers
+     * @param unreadyCustomers
+     * @param ticketBoothAverage
+     * @param totalTicketCount
+     * @param wristbandAverageTime
+     * @param ticketAverageTime
+     * @param wholeAverageTime
+     * @param ticketWristbandTimeRatio
+     */
     public void addServicePoint(double simulationTime, int readyCustomers, int ticketCustomers, int wristbandCustomers, int unreadyCustomers, double ticketBoothAverage, int totalTicketCount, double wristbandAverageTime, double ticketAverageTime, double wholeAverageTime, double ticketWristbandTimeRatio){
         Connection conn = DatabaseConnection.getConnection();
         String sql = "INSERT INTO service_point (sim_id, simulation_time, ready_customers, ticket_customers, wristband_customers, unready_customers, ticket_booth_average, total_ticket_count, wristband_average_time, ticket_average_time, whole_average_time, ticket_wristband_time_ratio) VALUES (LAST_INSERT_ID(),?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
@@ -192,6 +301,16 @@ public class Dao {
         }
     }
 
+    /**
+     * Adds a ride to the database.
+     *
+     * @param rideId
+     * @param count
+     * @param averageServeTime
+     * @param averageWaitTime
+     * @param variance
+     * @param mean
+     */
     public void addRide(int rideId, int count, double averageServeTime, double averageWaitTime, double variance, double mean){
         Connection conn = DatabaseConnection.getConnection();
         String sql = "INSERT INTO ride (sim_id, ride_id, count, average_service_time, average_queue_time, variance, mean) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?, ?, ?);";
@@ -210,6 +329,14 @@ public class Dao {
         }
     }
 
+    /**
+     * Adds a ticket booth to the database.
+     *
+     * @param count
+     * @param ticketBoothId
+     * @param averageServiceTime
+     * @param averageQueueTime
+     */
     public void addTicketBooth(int count, int ticketBoothId, double averageServiceTime, double averageQueueTime){
         Connection conn = DatabaseConnection.getConnection();
         String sql = "INSERT INTO ticket_booth (sim_id, ticket_booth_id, count, average_service_time, average_queue_time) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?);";
@@ -226,6 +353,14 @@ public class Dao {
         }
     }
 
+    /**
+     * Adds a restaurant to the database.
+     *
+     * @param count
+     * @param averageServiceTime
+     * @param averageQueueTime
+     * @param capacity
+     */
     public void addRestaurant(int count, double averageServiceTime, double averageQueueTime, int capacity){
         Connection conn = DatabaseConnection.getConnection();
         String sql = "INSERT INTO restaurant (sim_id, count, average_service_time, average_queue_time, capacity) VALUES (LAST_INSERT_ID(), ?, ?, ?, ?);";
